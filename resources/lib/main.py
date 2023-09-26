52cddd13c616454e0ccfa1ba296848afe7ba4d16
Lavish <64689132+lavish440@users.noreply.github.com>
2023-09-26 17:52:37 +0530

    Added the fix for 480p max resolution for some channels

--- resources/lib/main.py
+++ resources/lib/main.py
@@ -430,7 +430,9 @@ def play(
         headers["srno"] = str(uuid4()) if "srno" not in rjson else rjson["srno"]
         enableHost = Settings.get_boolean("enablehost")
         res = urlquick.post(
-            GET_CHANNEL_URL,
+            GET_CHANNEL_URL=GET_CHANNEL_URL.replace(0, 1)
+            if broadcasterid in [3, 6]
+            else GET_CHANNEL_URL,
             json=rjson,
             verify=False,
             headers=getChannelHeadersWithHost() if enableHost else getChannelHeaders(),
